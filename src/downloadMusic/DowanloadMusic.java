7487db3a75dbdb4ee5cb2fa7638782d7a87ec5ab
Влад <fredomworld1@gmail.com>
2017-12-16 12:16:49 +0300

    создан поток для скачивания музыки

---
+++ src/downloadMusic/DowanloadMusic.java
@@ -0,0 +1,60 @@
+package downloadMusic;
+
+import java.io.*;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class DowanloadMusic extends Thread {
+
+    private static final String IN_FILE_TXT = "/home/juja/IdeaProjects/TheadDemo/src/downloadMusic/inFile.txt";
+    private static final String OUT_FILE_TXT = "/home/juja/IdeaProjects/TheadDemo/src/downloadMusic/outFile.txt";
+    private static final String PATH_TO_MUSIC = "/home/juja/IdeaProjects/TheadDemo/src/downloadMusic/music/";
+
+    @Override
+    public void run() {
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))) {
+            String music;
+            int count = 0;
+            try {
+                while ((music = musicFile.readLine()) != null) {
+                    new Download(music, PATH_TO_MUSIC + String.valueOf(count) + ".mp3").start();
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String Url;
+        String result;
+        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
+            while ((Url = inFile.readLine()) != null) {
+                URL url = new URL(Url);
+
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                    while ((result = bufferedReader.lines().collect(Collectors.joining("\n"))) != null) {
+
+                        Pattern email_pattern = Pattern.compile("\\s*(?<=data-url\\s?=\\s?\")[^>]*\\/*(?=\")");
+                        Matcher matcher = email_pattern.matcher(result);
+                        while (matcher.find()) {
+                            outFile.write(matcher.group() + "\r\n");
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DowanloadMusic music = new DowanloadMusic();
+        //music.start();
+    }
+
+}
---
+++ src/downloadMusic/Download.java
@@ -0,0 +1,58 @@
+package downloadMusic;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class Download extends Thread {
+    private   String strUrl;
+    private   String file;
+
+    public Download(String strUrl,String file) {
+        this.strUrl = strUrl;
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+
+            URL url = null;
+            try {
+                url = new URL(strUrl);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        ReadableByteChannel byteChannel = null;
+        try {
+            byteChannel = Channels.newChannel(url.openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileOutputStream stream = null;
+            try {
+                stream = new FileOutputStream(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        try {
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+                byteChannel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+}
