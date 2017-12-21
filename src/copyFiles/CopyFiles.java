8ff9c4f6daa4fe9024cd567c4e492d015993b189
Vlad <fredomworld1@gmail.com>
2017-12-21 15:34:04 +0300

    Произведен рефакторинг двух классов задачи
    Дубинин

--- src/copyFiles/CopyFiles.java
+++ src/copyFiles/CopyFiles.java
@@ -1,29 +1,35 @@
 package copyFiles;
-
+/**
+ * Класс для задачи копирование файлов
+ * @author Dubinin. 15it18
+ */
 import java.io.*;
-import java.sql.Time;
 
 public class CopyFiles extends Thread {
     private String adresSource;
     private String adresRecipteon;
-
+    private long timeRun;
 
     CopyFiles(String adresSource, String adresRecipteon) {
         this.adresSource = adresSource;
         this.adresRecipteon = adresRecipteon;
     }
-
+    long getTimeRun(){
+        return timeRun;
+    }
 
     @Override
     public void run() {
-        long time = System.currentTimeMillis();
+        long time=System.currentTimeMillis();
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(adresRecipteon));
              BufferedReader bufferedReader = new BufferedReader(new FileReader(adresSource))) {
 
             String copyLine;
             while ((copyLine = bufferedReader.readLine()) != null) {
+
                 bufferedWriter.write(copyLine + '\n');
             }
+            timeRun=System.currentTimeMillis()-time;
         } catch (IOException e) {
 
         }
--- src/copyFiles/Main.java
+++ src/copyFiles/Main.java
@@ -1,15 +1,86 @@
 package copyFiles;
 
+/**
+ * Класс для задачи копирование файлов
+ * @author Dubinin. 15it18
+ */
 public class Main {
-    private  static final String ADRESS="/home/juja/IdeaProjects/TheadDemo/src/copyFiles/Files";
+    private static final String ADRESS = "F:\\TheadDemo\\src\\copyFiles\\Files.txt";
+
     public static void main(String[] args) throws InterruptedException {
-      CopyFiles copyFiles =new CopyFiles(ADRESS, "/home/juja/IdeaProjects/TheadDemo/src/copyFiles/Files(copy).txt");
-      CopyFiles copyFiles1 =new CopyFiles(ADRESS, "/home/juja/IdeaProjects/TheadDemo/src/copyFiles/Files(copy2).txt");
-        long time= System.currentTimeMillis();
-      copyFiles.start();
-      copyFiles.join();
-      copyFiles1.start();
-      copyFiles1.join();
-        System.out.println(System.currentTimeMillis()- time);
+
+        CopyFiles copyFiles = new CopyFiles(ADRESS, "F:\\TheadDemo\\src\\copyFiles\\Files(copy).txt");
+        CopyFiles copyFiles1 = new CopyFiles(ADRESS, "F:\\TheadDemo\\src\\copyFiles\\Files(copy2).txt");
+
+        run(copyFiles);
+        joinThread(copyFiles);
+
+        System.out.println("Задание первое - " + copyFiles.getTimeRun());
+
+        copyFiles = new CopyFiles(ADRESS, "F:\\TheadDemo\\src\\copyFiles\\Files(copy).txt");
+
+        run(copyFiles);
+        joinThread(copyFiles);
+
+        run(copyFiles1);
+        joinThread(copyFiles1);
+
+        System.out.println("Задание второе - " + (copyFiles.getTimeRun() + copyFiles1.getTimeRun()));
+
+        copyFiles = new CopyFiles(ADRESS, "F:\\TheadDemo\\src\\copyFiles\\Files(copy).txt");
+        copyFiles1 = new CopyFiles(ADRESS, "F:\\TheadDemo\\src\\copyFiles\\Files(copy2).txt");
+
+        run(copyFiles, copyFiles1);
+        joinThread(copyFiles, copyFiles1);
+
+        System.out.print("Задание третье: ");
+        System.out.print(copyFiles.getTimeRun());
+        System.out.print(","+copyFiles1.getTimeRun());
+
+
+    }
+
+    /**
+     * Метод запускает 2 потока.
+     *
+     * @param copyFiles  1 поток
+     * @param copyFiles1 2 поток
+     */
+    private static void run(CopyFiles copyFiles, CopyFiles copyFiles1) {
+        copyFiles.start();
+        copyFiles1.start();
+
+    }
+
+    /**
+     * Метод запускает один поток
+     *
+     * @param copyFiles поток
+     */
+    private static void run(CopyFiles copyFiles) {
+        copyFiles.start();
+    }
+
+    /**
+     * Метод вызывает метод join() для потоков, который ждет пока выполниться поток.
+     *
+     * @param copyFiles  1 поток
+     * @param copyFiles1 2 поток
+     * @throws InterruptedException исключение.
+     */
+    private static void joinThread(CopyFiles copyFiles, CopyFiles copyFiles1) throws InterruptedException {
+        copyFiles.join();
+        copyFiles1.join();
+    }
+
+    /**
+     * метод вызывает метод join() для потока, который ждет пока поток выпониться.
+     *
+     * @param copyFiles поток.
+     * @throws InterruptedException исключение.
+     */
+    private static void joinThread(CopyFiles copyFiles) throws InterruptedException {
+        copyFiles.join();
     }
 }
+
